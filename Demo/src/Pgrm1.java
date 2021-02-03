e36fb4c1fdcf1764c1e48483f912c49e8366e948
venkatesh9298 <ivenkateshotw@gmail.com>
2021-02-03 12:19:47 +0530

    done

--- Demo/src/Pgrm1.java
+++ Demo/src/Pgrm1.java
@@ -1,24 +1,28 @@
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+// hello
 public class Pgrm1 {
 
-	public static void main(String[] args) throws SQLException {
-	 
-		Driver d = new oracle.jdbc.driver.OracleDriver();
-        DriverManager.registerDriver(d);
-        Connection con =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","babu");
-	    Statement st=con.createStatement();
-	   
-	    st.executeUpdate("create table bl1(sno number(3))");
-	    
-	    con.close();
-	    System.out.println("success");
-     }
+  public static void main(String[] args) throws SQLException {
 
-	
-	
+    Driver d = new oracle.jdbc.driver.OracleDriver();
+    DriverManager.registerDriver(d);
+    Connection con =
+        DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "babu");
+    Statement st = con.createStatement();
 
-	
+    st.executeUpdate("create table bl1(sno number(3))");
+
+    con.close();
+    System.out.println("success");
+  }
+
+
+
 }
 
---
+++ myApp/src/main/java/com/mv/myApp/App.java
@@ -0,0 +1,13 @@
+package com.mv.myApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
